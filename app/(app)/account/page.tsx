e3f965806c3b4f711180bb13bc0cf651de87c554
Augustine Godwin <augustinegodwin312@gmail.com>
2026-03-27 15:55:11 +0100

    same as the last one

--- app/(app)/account/page.tsx
+++ app/(app)/account/page.tsx
@@ -1,5 +1,5 @@
 import MaxWidthContainer from "@/app/components/utils/maxWidthContainer";
-import bot from "@/app/assets/images/bot.png";
+import bot from "@/app/assets/images/user.jpg";
 import Image from "next/image";
 import Input from "@/app/components/utils/input";
 import FilterCard from "@/app/components/utils/filterCard";
@@ -19,15 +19,16 @@ export default function Page() {
                 </div>
                 <div className="flex flex-row lg:flex-col gap-4 w-full overflow-hidden">
                   <FilterCard active={true} title="General Settings" />
-                <FilterCard active={false} title="Wallet" />
-                <FilterCard active={false} title="Products" />
-                <FilterCard active={false} title="Orders" />
-                <FilterCard active={false} title="Additional Settings" />
+                  <FilterCard active={false} title="Wallet" />
+                  <FilterCard active={false} title="Products" />
+                  <FilterCard active={false} title="Orders" />
+                  <FilterCard active={false} title="Additional Settings" />
                 </div>
               </div>
-              <div className="flex-1 bg-(--card) rounded-[30px] h-auto border border-gray-100 p-1.5">
-                <div className="w-full p-5 bg-[#fcfcfc] border border-gray-100 rounded-3xl flex flex-col gap-10">
-                  <section className="w-full grid gap-x-8 gap-y-6 sm:grid-cols-2">
+              <div className="flex-1 ">
+                <div className="w-full py-5 px-1 flex flex-col gap-10">
+                  <>
+                    <section className="w-full grid gap-x-8 gap-y-6 sm:grid-cols-2">
                     <div className="space-y-1">
                       <h2 className="text-lg tracking-body text-(--primary) font-medium title-font">
                         Profile Image
@@ -36,7 +37,7 @@ export default function Page() {
                         This will be displayed on your product listing.
                       </p>
                     </div>
-                    <div className="size-20 rounded-full border border-gray-100">
+                    <div className="size-20 rounded-full overflow-hidden border border-gray-100">
                       <Image className="size-full" alt="logo" src={bot} />
                     </div>
                   </section>
@@ -123,8 +124,22 @@ export default function Page() {
                       </div>
                     </section>
                   </div>
+                  </>
                 </div>
               </div>
+              {/* <div className="flex-1 flex flex-col gap-5">
+                <div className="flex-1 flex flex-row gap-5">
+                  <div className="flex-1 h-fit bg-(--card) rounded-[30px]  border border-gray-100 p-1.5">
+                    <div className="w-full h-50 p-5 bg-[#fcfcfc] border border-gray-100 rounded-3xl flex flex-col gap-10"></div>
+                  </div>
+                  <div className="flex-1 h-fit bg-(--card) rounded-[30px]  border border-gray-100 p-1.5">
+                    <div className="w-full h-50 p-5 bg-[#fcfcfc] border border-gray-100 rounded-3xl flex flex-col gap-10"></div>
+                  </div>
+                  <div className="flex-1 h-fit bg-(--card) rounded-[30px]  border border-gray-100 p-1.5">
+                    <div className="w-full h-50 p-5 bg-[#fcfcfc] border border-gray-100 rounded-3xl flex flex-col gap-10"></div>
+                  </div>
+                </div>
+              </div> */}
             </div>
           </div>
         </div>
