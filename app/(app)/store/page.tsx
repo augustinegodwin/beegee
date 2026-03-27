d25e459e337b7d6da705a8bd8039a3975dc743ad
Augustine Godwin <augustinegodwin312@gmail.com>
2026-03-27 16:10:17 +0100

    made the store a bit responsive and same commit message  as the last one

--- app/(app)/store/page.tsx
+++ app/(app)/store/page.tsx
@@ -1,7 +1,8 @@
+import Button from '@/app/components/utils/button'
 import FilterCard from '@/app/components/utils/filterCard'
 import MaxWidthContainer from '@/app/components/utils/maxWidthContainer'
 import ProductCard from '@/app/components/utils/productCard'
-import SectionHeader from '@/app/components/utils/sectionHeader'
+
 import SwitchCard from '@/app/components/utils/switchCard'
 const filterItems=[
   {
@@ -32,14 +33,25 @@ const filterItems=[
 export default function Page() {
   return (
     <MaxWidthContainer>
-      <div className="w-full mt-10 py-25 bg-foreground border border-gray-100 rounded-3xl">
-
-        <SectionHeader
-            title='Bingham’s University biggest Student Market'
-            body='Discover to buy, sell and rent anything on campus with ease.'
-            buttonValue='Add your Stuff'
-            buttonAction='/store'
-        />
+      <div className="w-full mt-10 py-25 p-5 bg-foreground border border-gray-100 rounded-3xl">
+        <div className="w-full flex justify-center">
+              <div className="w-full items-center justify-center max-w-150 flex flex-col gap-5">
+                <div className="w-fit">
+                <h2 className="text-center title-font font-bold text-[32px] sm:text-5xl lg:text-[65px] text-(--primary) leading-none tracking-header">
+                    Bingham’s University biggest Student Market
+                  </h2>
+                </div>
+                <div className="w-full max-w-100 flex flex-col justify-center items-center gap-5">
+                  <p className="text-center text-lg text-(--secondary) leading-body title-font font-medium tracking-body">
+                    Discover to buy, sell and rent anything on campus with ease.
+                  </p>
+                
+                    <Button title={"Add your Stuff"} LinkTo={"/store"} />
+                 
+                </div>
+              </div>
+            </div>
+        
       </div>
       <div className='w-full py-25 gap-5 flex flex-col'>
             <div className='w-full flex gap-5 flex-row'>
--- app/components/utils/feedbackCard.tsx
+++ app/components/utils/feedbackCard.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function FeedbackCard() {
   return (
-    <div className="w-full bg-(--card) flex flex-col p-5 rounded-[20px] gap-6">
+    <div className="w-full bg-(--card) flex flex-col p-4 sm:p-5 rounded-[20px] gap-6">
         <div className="w-full">
             <p className="leading-body title-font track-body font-medium text-(--secondary)">The energy blend is now part of my morning routine. Love the all-natural approach.</p>
         </div>
